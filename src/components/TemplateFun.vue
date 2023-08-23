6cb8c643a2336af7b62f32b63a07b7b046a73d33
Njikem <123434988+Njikem@users.noreply.github.com>
2023-08-23 07:32:18 -0600

    binding

---
+++ src/components/TemplateFun.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h3>{{blog_title}}</h3>
+        <p>{{blog_content}}</p>
+        <img :src="blog_image_url"/>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                blog_content: "Do not bother of what others are saying about you, own your world",
+                blog_title: "Story of my life",
+                blog_image_url: 'https://unsplash.com/s/photos/life',
+
+                
+            }
+        },
+        
+    }
+</script>
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template-fun></template-fun>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import TemplateFun from '@/components/TemplateFun.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  TemplateFun,
   }
 }
 </script>
